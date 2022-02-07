25438c9d72aa14934cf2fd44cc15158feaac511a
Rhiali Cândido <rhiali.candido@gmail.com>
2022-02-06 23:02:04 -0400

    fix: Fixed project structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,1 @@
-/*
-|--------------------------------------------------------------------------
-| Mix Asset Management
-|--------------------------------------------------------------------------
-|
-*/
-
-const mix = require('laravel-mix')
-
-mix.js('./radiox.js', 'dist/radiox.min.js')
-mix.css('./radiox.css', 'dist/radiox.min.css')
+require('laravel-mix').js('./radiox.js', 'dist/radiox.min.js').css('./radiox.css', 'dist/radiox.min.css')
